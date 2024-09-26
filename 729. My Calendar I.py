7291f6544b77a2ed33a71be30e0f710056f412c1
keithxun <keithonghx@gmail.com>
2024-09-27 00:24:22 +0800

    Use of interval tree to solve the problem. Fast af runtime

---
+++ 729. My Calendar I.py
@@ -0,0 +1,38 @@
+class TreeNode():
+    def __init__(self, s, e):
+        self.s = s
+        self.e = e
+        self.left = None
+        self.right = None
+
+class MyCalendar:
+    def __init__(self):
+        self.root = None
+
+    def book(self, start: int, end: int) -> bool:
+        if not self.root:
+            self.root = TreeNode(start, end)
+            return True
+        else:
+            return self.insert(start, end, self.root)
+
+    def insert(self, s, e, node):
+        if s >= node.e:
+            if node.right:
+                return self.insert(s, e, node.right)
+            else:
+                node.right = TreeNode(s, e)
+                return True
+        elif e <= node.s:
+            if node.left:
+                return self.insert(s, e, node.left)
+            else:
+                node.left = TreeNode(s, e)
+                return True
+        else:
+            return False
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
