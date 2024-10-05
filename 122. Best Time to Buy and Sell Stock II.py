aded448dcc98d6d977d887408a1dddcbb37767e9
keithxun <keithonghx@gmail.com>
2024-10-05 11:31:29 +0800

    Add greedy approach

--- 122. Best Time to Buy and Sell Stock II.py
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -9,3 +9,16 @@ class Solution:
             dp[day, False] = max(dp[day - 1, False], dp[day - 1, True] + price)
         
         return dp[len(prices) - 1, False]
+    
+# Even faster solution through greedy approach
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+
+        # Traverse through prices list
+        for i in range(1, len(prices)):
+            # If the price on day i is higher than day i-1, we take the profit
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+
+        return max_profit
