3ffb5f773b1c77a5b0171c555020fb7f28ee926f
keithxun <keithonghx@gmail.com>
2024-10-06 17:43:37 +0800

    Use of custom compare functions and types to concantenate strings and compare them numerically.

---
+++ 179. Largest Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        def cmp_f(x, y):
+            if x + y > y + x: # Concetenate string
+                return 1
+            elif x == y:
+                return 0
+            else:
+                return -1
+        
+        nums = [str(num) for num in nums]
+
+        nums.sort(key = cmp_to_key(cmp_f), reverse = True)
+
+        return ''.join(nums).lstrip('0') or '0'
